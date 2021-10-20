e7cb618b31f3d4eee7ee2807e6f8ad7a73985f46
Huong Nguyen <huongnguyen10102@gmail.com>
2021-10-21 00:02:03 +0300

    fetch products fixed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React, { useState, useEffect} from 'react';
-import './App.css';
-import Products from './components/Products';
+import React from 'react';
+import Products from "./components/Products";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
   /* fetch products */
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then((response) => response.json())
-        .then((products) => setProducts(products));
-  }, []);
-  /* end fetch products */
-
-  return(
-    <div className="container">
-      <h1>Ecommerce shopping cart</h1>
-      <hr/>
-      <div className="row">
-        <div className="col-sm-8">
-          <Products/>
-        </div>
-        <div className="col-sm-4">
-          cart
+  componentDidMount() {
+    fetch("https://fakestoreapi.com/products")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  render() {
+    return(
+      <div className="container">
+        <h1>Ecommerce Shopping Cart</h1>
+        <hr/>
+        <div className="row">
+          <div className="col-sm-8">
+            <Products/>
+          </div>
+          <div className="col-sm-4">
+            cart
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
